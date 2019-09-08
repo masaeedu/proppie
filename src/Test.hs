1948382b65c6693b63fc0867366d50bfbadb7794
Asad Saeeduddin <masaeedu@gmail.com>
2019-09-08 04:48:58 -0400

    Add working mix of lens + iso

--- src/Test.hs
+++ src/Test.hs
@@ -2,7 +2,7 @@ module Test where
 
 import Data.Coerce
 
-import Pipes (Server', Client', request, request_, respond, (>>>), runEffect)
+import Pipes (Server', Client, request, respond, (>>>), runEffect)
 import Data.Foldable
 import Control.Monad
 import Optics
@@ -12,19 +12,15 @@ import ExtraClasses
 server :: Functor m => Server' Int m r
 server = forever $ respond (* 2)
 
-client :: Int -> Client' (Int, Int) IO ()
+client :: Show s => Int -> Client s (Int, Int) IO ()
 client l = traverse_ go [0..l]
   where
   go i = do
     res <- request (i, i)
     mpure $ print res
 
-server' :: Functor m => Server' (Int, Int) m r
-server' = coerce $ ExtraClasses.zip (Downstream server) (Downstream server)
-
 main :: IO ()
-main = do
-  runEffect $ (coerce $ flipped . _1 $ Downstream $ server) >>> client 10
+main = runEffect $ (coerce $ flipped . _1 $ Downstream $ server) >>> client 10
 {-
 *Test> main
 (0,0)
